f2ffd2cda6136a0166f38c2fada72297c206c7f3
alireza Morovati <fazel.accont@gmail.com>
2025-07-10 00:01:35 +0000

    هنوز هیچ اطلاعاتی نمایش داده نمیشود

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -13,7 +13,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-// Firebase Storage is not initialized because it's unavailable in the user's region.
-// const storage = getStorage(app);
 
 export { app, db };
